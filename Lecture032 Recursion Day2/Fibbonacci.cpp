abc6ed622d2fc2df48c112fc7b611f1a473d1ee7
yanuj09 <anujyadav.itengineer@gmail.com>
2024-10-17 21:18:49 +0530

    Update Fibbonacci.cpp
    updated the fibnocci problem using recursion

--- Lecture032 Recursion Day2/Fibbonacci.cpp
+++ Lecture032 Recursion Day2/Fibbonacci.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 
+int fib(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+
+        int ans = fib(n-1) + fib(n-2);
+
+        return ans;
+    }
+
 int main() {
 
 
